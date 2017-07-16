fcd2d6db5e060cbe5648ac3e58b056d937f6399e
seanphunt18 <seanphunt18@gmail.com>
2017-07-16 15:40:20 -0400

    correct test and html files

--- test/entry-test.js
+++ test/entry-test.js
@@ -1,29 +1,22 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-
-const {app, runServer, closeServer} = require('../server');
+const server = require('../server.js');
 
 const should = chai.should();
+const app = server.app;
+const storage = server.storage;
 
 chai.use(chaiHttp);
 
-describe('Index Page', function() {
 
-	before(function() {
-    	return runServer();
-  	});
-
-  	after(function() {
-    	return closeServer();
-  	});
-	
-	it('should return code 200 and some HTML', function() {
-
-	    return chai.request(app)
-	      .get('/')
-	      .then(function(res) {
-	        res.should.have.status(200);
-	        res.body.should.be.html;
-	    });
+describe('index page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
     });
-}); 
+  });
+});
