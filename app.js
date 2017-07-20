3f7e2164c5a202b5156921c3fbb4df2c99638cc0
seanphunt18 <seanphunt18@gmail.com>
2017-07-19 21:44:22 -0400

    mock data display successful

--- app.js
+++ app.js
@@ -3,48 +3,66 @@ var MOCK_ENTRIES = {
 	"entries": [
 		{
 			"id": "111111",
-			"charity-name": "UNICEF",
-			"donation-amount": "$5",
-			"donation-type": "clothing"
+			"charity": "UNICEF",
+			"amount": "$5",
+			"type": "clothing",
+			"url": "http://google.com"
 		},
 		{
 			"id": "222222",
-			"charity-name": "UNICEF",
-			"donation-amount": "$10",
-			"donation-type": "food"
+			"charity": "UNICEF",
+			"amount": "$10",
+			"type": "food",
+			"url": "#"
 		},
 		{
 			"id": "333333",
-			"charity-name": "UNICEF",
-			"donation-amount": "$15",
-			"donation-type": "service"
+			"charity": "UNICEF",
+			"amount": "$15",
+			"type": "service",
+			"url": "#"
 		},
 		{
 			"id": "444444",
-			"charity-name": "UNICEF",
-			"donation-amount": "$20",
-			"donation-type": "infrastructure"
+			"charity": "UNICEF",
+			"amount": "$20",
+			"type": "infrastructure",
+			"url": "#"
 		},
 		{
 			"id": "555555",
-			"charity-name": "UNICEF",
-			"donation-amount": "$25",
-			"donation-type": "education"
+			"charity": "UNICEF",
+			"amount": "$25",
+			"type": "education",
+			"url": "#"
 		},
 		{
 			"id": "666666",
-			"charity-name": "UNICEF",
-			"donation-amount": "$30",
-			"donation-type": "medical"
+			"charity": "UNICEF",
+			"amount": "$30",
+			"type": "medical",
+			"url": "#"
 		},
 	]
 };
 
+function getDisplayEntries(callbackFn) {
+	setTimeout(function(){ callbackFn(MOCK_ENTRIES)}, 100);
+}
+
 function displayEntries(data) {
 	for (index in data.entries) {
 		$('.results').append(
-			'<div class="result-container"><div class="result-price"><h1>' + data.entries[index].price + '</h1></div><div class="result-info"><h3>' + data.entries[index].charity-name + '</h3><p>Service provided: ' + data.entries[index].donation-type + '</p></div></div>'
+			'<div class="result-container"><div class="result-price"><h1>' + data.entries[index].amount + '</h1></div><div class="result-info"><h3>' + data.entries[index].charity + '</h3><p>Service provided: ' + data.entries[index].type + '</p><div class="donate"><a href="' + data.entries[index].url + '" target="_blank">Donate</a></div></div></div>'
 		);
 	}
+};
+
+function watchFilter() {
+	$('.js-form').submit(function(e) {
+		e.preventDefault();
+		getDisplayEntries(displayEntries);
+	});
 }
 
+$(function(){watchFilter();});
